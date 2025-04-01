36eb5a1f09b8a707346bcf16e59503b2c8e0338c
dakshhpanchal <dakshpanchal08@gmail.com>
2025-04-01 20:09:56 +0530

    object avoidance with waypoint navigation

--- src/lane_detection/scripts/test_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-model_path = "/home/daksh/Robotics_ws/src/lane_detection/scripts/model.h5"
-print(f"🔍 Loading model from: {model_path}")
-try:
-    model = tf.keras.models.load_model(model_path)
-    print("✅ Model loaded successfully!")
-except Exception as e:
-    print(f"❌ Error loading model: {e}")
-
